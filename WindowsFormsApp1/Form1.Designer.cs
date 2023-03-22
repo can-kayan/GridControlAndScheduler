4bd54afad9e57f746db5d88ae261dfdba45176ea
Cank01 <canm2064@gmail.com>
2023-03-23 00:14:11 +0300

    GridControlAndScheduler
    Opening a GridControl-Scheduler Appointment Page

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler4 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler5 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler6 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.weddingAppDataSet = new WindowsFormsApp1.WeddingAppDataSet();
+            this.appointmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appointmentsTableAdapter = new WindowsFormsApp1.WeddingAppDataSetTableAdapters.AppointmentsTableAdapter();
+            this.colUniqueID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStartDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEndDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAllDay = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSubject = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colLocation = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDescription = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStatus = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colLabel = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colResourceID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colResourceIDs = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colReminderInfo = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colRecurrenceInfo = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTimeZoneId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.resourcesTableAdapter = new WindowsFormsApp1.WeddingAppDataSetTableAdapters.ResourcesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weddingAppDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Month;
+            this.schedulerControl1.DataStorage = this.schedulerStorage1;
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(800, 200);
+            this.schedulerControl1.Start = new System.DateTime(2023, 3, 20, 0, 0, 0, 0);
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler4);
+            this.schedulerControl1.Views.FullWeekView.Enabled = true;
+            this.schedulerControl1.Views.FullWeekView.TimeRulers.Add(timeRuler5);
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler6);
+            // 
+            // schedulerStorage1
+            // 
+            this.schedulerStorage1.Appointments.DataSource = this.appointmentsBindingSource;
+            this.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay";
+            this.schedulerStorage1.Appointments.Mappings.Description = "Description";
+            this.schedulerStorage1.Appointments.Mappings.End = "EndDate";
+            this.schedulerStorage1.Appointments.Mappings.Label = "Label";
+            this.schedulerStorage1.Appointments.Mappings.Location = "Location";
+            this.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo";
+            this.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo";
+            this.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceID";
+            this.schedulerStorage1.Appointments.Mappings.Start = "StartDate";
+            this.schedulerStorage1.Appointments.Mappings.Status = "Status";
+            this.schedulerStorage1.Appointments.Mappings.Subject = "Subject";
+            this.schedulerStorage1.Appointments.Mappings.TimeZoneId = "TimeZoneId";
+            this.schedulerStorage1.Appointments.Mappings.Type = "Type";
+            this.schedulerStorage1.Resources.DataSource = this.resourcesBindingSource;
+            this.schedulerStorage1.Resources.Mappings.Caption = "ResourceName";
+            this.schedulerStorage1.Resources.Mappings.Color = "Color";
+            this.schedulerStorage1.Resources.Mappings.Id = "ResourceID";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.appointmentsBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 200);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(800, 250);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.DoubleClick += new System.EventHandler(this.gridControl1_DoubleClick);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colUniqueID,
+            this.colStartDate,
+            this.colEndDate,
+            this.colAllDay,
+            this.colSubject,
+            this.colLocation,
+            this.colDescription,
+            this.colStatus,
+            this.colLabel,
+            this.colResourceID,
+            this.colResourceIDs,
+            this.colReminderInfo,
+            this.colRecurrenceInfo,
+            this.colTimeZoneId});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            // 
+            // weddingAppDataSet
+            // 
+            this.weddingAppDataSet.DataSetName = "WeddingAppDataSet";
+            this.weddingAppDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // appointmentsBindingSource
+            // 
+            this.appointmentsBindingSource.DataMember = "Appointments";
+            this.appointmentsBindingSource.DataSource = this.weddingAppDataSet;
+            // 
+            // appointmentsTableAdapter
+            // 
+            this.appointmentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // colUniqueID
+            // 
+            this.colUniqueID.FieldName = "UniqueID";
+            this.colUniqueID.Name = "colUniqueID";
+            this.colUniqueID.Visible = true;
+            this.colUniqueID.VisibleIndex = 0;
+            // 
+            // colStartDate
+            // 
+            this.colStartDate.FieldName = "StartDate";
+            this.colStartDate.Name = "colStartDate";
+            this.colStartDate.Visible = true;
+            this.colStartDate.VisibleIndex = 2;
+            // 
+            // colEndDate
+            // 
+            this.colEndDate.FieldName = "EndDate";
+            this.colEndDate.Name = "colEndDate";
+            this.colEndDate.Visible = true;
+            this.colEndDate.VisibleIndex = 3;
+            // 
+            // colAllDay
+            // 
+            this.colAllDay.FieldName = "AllDay";
+            this.colAllDay.Name = "colAllDay";
+            this.colAllDay.Visible = true;
+            this.colAllDay.VisibleIndex = 4;
+            // 
+            // colSubject
+            // 
+            this.colSubject.FieldName = "Subject";
+            this.colSubject.Name = "colSubject";
+            this.colSubject.Visible = true;
+            this.colSubject.VisibleIndex = 5;
+            // 
+            // colLocation
+            // 
+            this.colLocation.FieldName = "Location";
+            this.colLocation.Name = "colLocation";
+            this.colLocation.Visible = true;
+            this.colLocation.VisibleIndex = 6;
+            // 
+            // colDescription
+            // 
+            this.colDescription.FieldName = "Description";
+            this.colDescription.Name = "colDescription";
+            this.colDescription.Visible = true;
+            this.colDescription.VisibleIndex = 7;
+            // 
+            // colStatus
+            // 
+            this.colStatus.FieldName = "Status";
+            this.colStatus.Name = "colStatus";
+            this.colStatus.Visible = true;
+            this.colStatus.VisibleIndex = 8;
+            // 
+            // colLabel
+            // 
+            this.colLabel.FieldName = "Label";
+            this.colLabel.Name = "colLabel";
+            this.colLabel.Visible = true;
+            this.colLabel.VisibleIndex = 9;
+            // 
+            // colResourceID
+            // 
+            this.colResourceID.FieldName = "ResourceID";
+            this.colResourceID.Name = "colResourceID";
+            this.colResourceID.Visible = true;
+            this.colResourceID.VisibleIndex = 10;
+            // 
+            // colResourceIDs
+            // 
+            this.colResourceIDs.FieldName = "ResourceIDs";
+            this.colResourceIDs.Name = "colResourceIDs";
+            this.colResourceIDs.Visible = true;
+            this.colResourceIDs.VisibleIndex = 11;
+            // 
+            // colReminderInfo
+            // 
+            this.colReminderInfo.FieldName = "ReminderInfo";
+            this.colReminderInfo.Name = "colReminderInfo";
+            this.colReminderInfo.Visible = true;
+            this.colReminderInfo.VisibleIndex = 12;
+            // 
+            // colRecurrenceInfo
+            // 
+            this.colRecurrenceInfo.FieldName = "RecurrenceInfo";
+            this.colRecurrenceInfo.Name = "colRecurrenceInfo";
+            this.colRecurrenceInfo.Visible = true;
+            this.colRecurrenceInfo.VisibleIndex = 13;
+            // 
+            // colTimeZoneId
+            // 
+            this.colTimeZoneId.FieldName = "TimeZoneId";
+            this.colTimeZoneId.Name = "colTimeZoneId";
+            this.colTimeZoneId.Visible = true;
+            this.colTimeZoneId.VisibleIndex = 14;
+            // 
+            // resourcesBindingSource
+            // 
+            this.resourcesBindingSource.DataMember = "Resources";
+            this.resourcesBindingSource.DataSource = this.weddingAppDataSet;
+            // 
+            // resourcesTableAdapter
+            // 
+            this.resourcesTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.schedulerControl1);
+            this.Name = "Form1";
+            this.Text = "sc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weddingAppDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private WeddingAppDataSet weddingAppDataSet;
+        private System.Windows.Forms.BindingSource appointmentsBindingSource;
+        private WeddingAppDataSetTableAdapters.AppointmentsTableAdapter appointmentsTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colUniqueID;
+        private DevExpress.XtraGrid.Columns.GridColumn colStartDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colEndDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colAllDay;
+        private DevExpress.XtraGrid.Columns.GridColumn colSubject;
+        private DevExpress.XtraGrid.Columns.GridColumn colLocation;
+        private DevExpress.XtraGrid.Columns.GridColumn colDescription;
+        private DevExpress.XtraGrid.Columns.GridColumn colStatus;
+        private DevExpress.XtraGrid.Columns.GridColumn colLabel;
+        private DevExpress.XtraGrid.Columns.GridColumn colResourceID;
+        private DevExpress.XtraGrid.Columns.GridColumn colResourceIDs;
+        private DevExpress.XtraGrid.Columns.GridColumn colReminderInfo;
+        private DevExpress.XtraGrid.Columns.GridColumn colRecurrenceInfo;
+        private DevExpress.XtraGrid.Columns.GridColumn colTimeZoneId;
+        private System.Windows.Forms.BindingSource resourcesBindingSource;
+        private WeddingAppDataSetTableAdapters.ResourcesTableAdapter resourcesTableAdapter;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'weddingAppDataSet.Resources' table. You can move, or remove it, as needed.
+            this.resourcesTableAdapter.Fill(this.weddingAppDataSet.Resources);
+            // TODO: This line of code loads data into the 'weddingAppDataSet.Appointments' table. You can move, or remove it, as needed.
+            this.appointmentsTableAdapter.Fill(this.weddingAppDataSet.Appointments);
+
+        }
+
+        private void gridControl1_DoubleClick(object sender, EventArgs e)
+        {
+            schedulerControl1.SelectedAppointments.Clear();
+            Appointment apt = null;
+            string SelectingDate = string.Empty;
+            if (gridView1.RowCount > 0)
+                SelectingDate = gridView1.GetFocusedRowCellValue("Subject").ToString();
+            int iD = -1;
+            for (int i = 0; i < schedulerStorage1.Appointments.Count; i++)
+            {
+                if (schedulerStorage1.Appointments[i].Subject == SelectingDate)
+                {
+                    iD = i;
+                }
+            }
+            if (iD != -1)
+                apt = schedulerControl1.DataStorage.Appointments[iD];
+            if (apt != null)
+                schedulerControl1.SelectedAppointments.Add(apt);
+            if (this.schedulerControl1.SelectedAppointments.Count > 0)
+            {
+                apt = this.schedulerControl1.SelectedAppointments[0];
+            }
+            else
+            {
+                apt = this.schedulerControl1.DataStorage.CreateAppointment(AppointmentType.Normal);
+                apt.Start = this.schedulerControl1.SelectedInterval.Start;
+                apt.End = this.schedulerControl1.SelectedInterval.End;
+            }
+            this.schedulerControl1.ShowEditAppointmentForm(apt);
+        }
+    }
+}
